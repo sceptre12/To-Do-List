0a5ff2437020caabfd411a490066ae68b7f8e1f7
unknown <xthomas@aac07c2.ad.fiu.edu>
2014-12-01 09:52:52 -0500

    Simple to do list

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(window).load(function(){
+	if(localStorage.length !== 0){
+	  for(a = 1 ; a <= localStorage.length; a++){
+	      $($('li')[a-1]).append(localStorage.getItem("item"+a));
+		}
+	}
+});
+
+
+$(document).ready(function(){
+
+	$('.carousel').carousel({
+  		interval: 3800
+	});
+	// function to clear local storage 
+	$($('button')[1]).click(function(){
+		localStorage.clear();
+		resetUl();
+	});
+	// function to save items into local storage
+	$($('button')[0]).click(function(){
+		var b = 0;
+		for( i = 0; i < $('li').length; i++){
+			b++;
+			if( $('li')[i].innerHTML != ""){
+			   	localStorage.setItem("item"+b, $('li')[i].innerHTML);
+			}else{
+				b--;
+			}
+		}
+	});
+});
+function resetUl(){
+	$('ul').empty().wrapInner("<li></li><li></li><li></li><li></li>");
+}
+
